4d21ac0a4c42606f532c55e846725d2983920fa4
ramzeth <ramzeth25@gmail.com>
2026-04-02 21:04:08 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -1,1 +1,10 @@
-from django.db import models
+from django.db import models  # noqa=F401
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
--- main.py
+++ main.py
@@ -1,7 +1,48 @@
 import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
+from db.models import Genre, Actor
+
+genres = (
+    "Western",
+    "Action",
+    "Dramma",
+)
+
+actors = (
+    ("George", "Klooney"),
+    ("Kianu", "Reaves"),
+    ("Scarlett", "Keegan"),
+    ("Will", "Smith"),
+    ("Jaden", "Smith"),
+    ("Scarlett", "Johansson"),
+)
 
 
 def main() -> QuerySet:
-    pass
+    for genre in genres:
+        Genre.objects.create(
+            name=genre,
+        )
+
+    for f_name, l_name in actors:
+        Actor.objects.create(
+            first_name=f_name,
+            last_name=l_name,
+        )
+
+    Genre.objects.filter(name="Dramma",).update(name="Drama")
+    Actor.objects.filter(
+        last_name="Klooney"
+    ).update(
+        last_name="Clooney"
+    )
+    Actor.objects.filter(
+        first_name="Kianu",
+        last_name="Reaves").update(
+        first_name="Keanu",
+        last_name="Reeves"
+    )
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
